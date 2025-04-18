c2631251a20128c6b718602b12381398fe331ce3
Akio Kanashiki <akio@mail.com>
2025-04-19 02:02:21 +0900

    modified

--- tsukumogami-aws/lib/tsukumogami-aws-stack.ts
+++ tsukumogami-aws/lib/tsukumogami-aws-stack.ts
@@ -1,5 +1,7 @@
 import { CfnOutput, Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
 import { FoundationModel, FoundationModelIdentifier } from "aws-cdk-lib/aws-bedrock";
+import { AllowedMethods, BehaviorOptions, CachePolicy, Distribution } from "aws-cdk-lib/aws-cloudfront";
+import { FunctionUrlOrigin, S3BucketOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
 import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 import { FunctionUrlAuthType, InvokeMode } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
@@ -15,7 +17,7 @@ export class TsukumogamiAwsStack extends Stack {
         const foundationModelId = FoundationModelIdentifier.AMAZON_NOVA_MICRO_V1_0;
         const foundationModel = FoundationModel.fromFoundationModelId(this, 'FoundationModel', foundationModelId);
 
-        // webapp statics
+        // webapp static contents
         const contents = new Bucket(this, 'WebappContents', {
             removalPolicy: RemovalPolicy.DESTROY,
             autoDeleteObjects: true,
@@ -23,7 +25,7 @@ export class TsukumogamiAwsStack extends Stack {
         new BucketDeployment(this, 'WebappContentsDeployment', {
             destinationBucket: contents,
             destinationKeyPrefix: '',
-            sources: [ Source.asset(`${__dirname}/../../tsukumogami-web/.output/public`) ],
+            sources: [Source.asset(`${__dirname}/../../tsukumogami-web/.output/public`)],
             memoryLimit: 4096,
             retainOnDelete: false,
         });
@@ -51,11 +53,31 @@ export class TsukumogamiAwsStack extends Stack {
             resources: [foundationModel.modelArn],
         }));
 
-        const webappEndpoint = webapp.addFunctionUrl({
-            authType: FunctionUrlAuthType.NONE,
-            invokeMode: InvokeMode.BUFFERED,
+        // webapp endponts
+        const webappBehavior = {
+            origin: FunctionUrlOrigin.withOriginAccessControl(webapp.addFunctionUrl({
+                authType: FunctionUrlAuthType.AWS_IAM,
+                invokeMode: InvokeMode.BUFFERED,
+            })),
+            allowedMethods: AllowedMethods.ALLOW_ALL,
+        } as BehaviorOptions;
+        const s3BucketBehavior = {
+            origin: S3BucketOrigin.withOriginAccessControl(contents, { originPath: '', },),
+            allowedMethods: AllowedMethods.ALLOW_GET_HEAD,
+            cachePolicy: CachePolicy.CACHING_DISABLED,
+        } as BehaviorOptions;
+
+        const destination = new Distribution(this, 'WebappDistribution', {
+            defaultBehavior: webappBehavior,
+            additionalBehaviors: {
+                '/_nuxt/**': s3BucketBehavior,
+                '/robot.txt': s3BucketBehavior,
+                '/favicon.ico': s3BucketBehavior,
+                
+            }
         });
 
-        new CfnOutput(this, 'ApiEndpoint', { value: webappEndpoint.url, });
+        // print access url
+        new CfnOutput(this, 'ApiEndpoint', { value: `https://${destination.distributionDomainName}`, });
     }
 }
